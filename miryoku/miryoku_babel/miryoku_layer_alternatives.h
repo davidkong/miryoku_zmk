fb0a715f6bf76940eb2acee22d998acb8eb260ed
davidkong <2808926+davidkong@users.noreply.github.com>
2023-02-24 15:40:04 -0500

    Fix typo

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -98,7 +98,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_DVORAK \
 &kp SQT,           &kp COMMA,         &kp DOT,           &kp P,             &kp Y,             &kp F,             &kp G,             &kp C,             &kp R,             &kp L,             \
-U_MT(LSHFT, A),     U_MT(LALT, O),    U_MT(LCTRL, E),    U_MT(LGUI, U),     U_LT(U_MEDIA I),   &kp D,             U_MT(LGUI, H),     U_MT(LCTRL, T),    U_MT(LALT, N),     U_MT(LSHFT, S),     \
+U_MT(LSHFT, A),     U_MT(LALT, O),    U_MT(LCTRL, E),    U_MT(LGUI, U),     U_LT(U_MEDIA, I),   &kp D,             U_MT(LGUI, H),     U_MT(LCTRL, T),    U_MT(LALT, N),     U_MT(LSHFT, S),     \
 U_LT(U_BUTTON, SLASH),U_MT(RALT, Q),  &kp J,             &kp K,             &kp X,             &kp B,             &kp M,             &kp W,             U_MT(RALT, V),     U_LT(U_BUTTON, Z), \
 U_NP,              U_NP,              U_MT(LSHFT, ESC),  U_LT(U_NAV, DEL),  U_LT(U_MOUSE, TAB),U_LT(U_FUN, RET),  U_LT(U_NUM, BSPC), U_LT(U_SYM, SPACE),U_NP,              U_NP
 
